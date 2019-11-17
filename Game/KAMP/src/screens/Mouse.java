d022d09752375ea8f32cf5d8bae44741f7c44179
connor-c1 <57877947+connor-c1@users.noreply.github.com>
2019-11-17 17:51:56 -0500

    Add files via upload
    The KAMP.png is the menu screen, the Type.png is the typing game screen, the Click.png is the clicking game screen. The BufferedImageLoader is my attempt at making the images load and I started a Mouse class to handle clicking actions.

---
+++ Game/KAMP/src/screens/Mouse.java
@@ -0,0 +1,41 @@
+package screens;
+
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+
+public class Mouse implements MouseListener{
+
+	@Override
+	public void mouseClicked(MouseEvent arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void mouseEntered(MouseEvent arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void mouseExited(MouseEvent arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void mousePressed(MouseEvent e) {
+		int mx = e.getX();
+		int my = e.getY();
+		
+		if(mx >=)
+		
+	}
+
+	@Override
+	public void mouseReleased(MouseEvent arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
